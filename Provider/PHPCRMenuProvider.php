242ef94b290fd7e6224da59933ee7b21ee26639b
Lukas Kahwe Smith <smith@pooteeweet.org>
2012-10-21 22:05:39 +0200

    there is no reason to filter by a class name here

--- Provider/PHPCRMenuProvider.php
+++ Provider/PHPCRMenuProvider.php
@@ -48,13 +48,12 @@ class PHPCRMenuProvider implements MenuProviderInterface
      * @param string $className the menu document class name. with phpcr-odm
      *      this can be null
      */
-    public function __construct(ContainerInterface $container, FactoryInterface $factory, $objectManagerName, $menuRoot, $className = null)
+    public function __construct(ContainerInterface $container, FactoryInterface $factory, $objectManagerName, $menuRoot)
     {
         $this->container = $container;
         $this->factory = $factory;
         $this->dm = $this->container->get('doctrine_phpcr')->getManager($objectManagerName);
         $this->menuRoot = $menuRoot;
-        $this->className = $className;
     }
 
     public function get($name, array $options = array())
@@ -62,7 +61,7 @@ class PHPCRMenuProvider implements MenuProviderInterface
         if (empty($name)) {
             throw new \InvalidArgumentException('The menu name may not be empty');
         }
-        $menu = $this->dm->find($this->className, $this->menuRoot . '/' . $name);
+        $menu = $this->dm->find(null, $this->menuRoot . '/' . $name);
         if ($menu === null) {
             throw new \InvalidArgumentException(sprintf('The menu "%s" is not defined.', $name));
         }
@@ -76,7 +75,7 @@ class PHPCRMenuProvider implements MenuProviderInterface
 
     public function has($name, array $options = array())
     {
-        $menu = $this->dm->find($this->className, $this->menuRoot . '/' . $name);
+        $menu = $this->dm->find(null, $this->menuRoot . '/' . $name);
         return $menu !== null;
     }
 }
