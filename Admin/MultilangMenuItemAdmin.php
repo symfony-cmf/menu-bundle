82a4a06099cbbdb469305ed2bc0d247906fd6a69
Emmanuel Vella <vella.emmanuel@gmail.com>
2012-12-31 11:13:28 +0100

    Add locale choice in admin

--- Admin/MultilangMenuItemAdmin.php
+++ Admin/MultilangMenuItemAdmin.php
@@ -5,8 +5,28 @@ namespace Symfony\Cmf\Bundle\MenuBundle\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\AdminBundle\Datagrid\ListMapper;
 
+use Symfony\Cmf\Bundle\MenuBundle\Document\MultilangMenuItem;
+
 class MultilangMenuItemAdmin extends MenuItemAdmin
 {
+    /**
+     * @var array
+     */
+    protected $locales;
+
+    /**
+     * @param string $code
+     * @param string $class
+     * @param string $baseControllerName
+     * @param array  $locales
+     */
+    public function __construct($code, $class, $baseControllerName, $locales)
+    {
+        parent::__construct($code, $class, $baseControllerName);
+
+        $this->locales = $locales;
+    }
+
     protected function configureListFields(ListMapper $listMapper)
     {
         parent::configureListFields($listMapper);
@@ -19,9 +39,29 @@ class MultilangMenuItemAdmin extends MenuItemAdmin
     {
         $formMapper
             ->with('General')
-                ->add('locale', 'text')
+                ->add('locale', 'choice', array(
+                    'choices' => array_combine($this->locales, $this->locales),
+                    'empty_value' => '',
+                ))
             ->end();
 
         parent::configureFormFields($formMapper);
+    }
+
+    public function getNewInstance()
+    {
+        /** @var $new MultilangMenuItem */
+        $new = parent::getNewInstance();
+
+        if ($this->hasRequest()) {
+            $currentLocale = $this->getRequest()->attributes->get('_locale');
+
+            if (in_array($currentLocale, $this->locales)) {
+                $meta = $this->getModelManager()->getMetadata(get_class($new));
+                $meta->setFieldValue($new, $meta->localeMapping, $currentLocale);
+            }
+        }
+
+        return $new;
     }
 }
