12a1970961cbca744de417a9ea745fcf52be7b07
dantleech <dan.t.leech@gmail.com>
2013-07-12 19:29:44 +0100

    Implemented test application

---
+++ Tests/Resources/DataFixtures/PHPCR/LoadMenuData.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Symfony\Cmf\Bundle\MenuBundle\Tests\Resources\DataFixtures\PHPCR;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Symfony\Cmf\Bundle\MenuBundle\Document\MenuNode;
+use Symfony\Cmf\Bundle\MenuBundle\Document\Menu;
+use Doctrine\ODM\PHPCR\Document\Generic;
+
+class LoadMenuData implements FixtureInterface, DependentFixtureInterface
+{
+    public function getDependencies()
+    {
+        return array(
+            'Symfony\Cmf\Component\Testing\DataFixtures\PHPCR\LoadBaseData',
+        );
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $root = $manager->find(null, '/test');
+        $menuRoot = new Generic;
+        $menuRoot->setNodename('menus');
+        $menuRoot->setParent($root);
+        $manager->persist($menuRoot);
+
+        $menu = new Menu;
+        $menu->setName('test-menu');
+        $menu->setLabel('Test Menu');
+        $menu->setParent($menuRoot);
+        $manager->persist($menu);
+
+        $menuNode = new MenuNode;
+        $menuNode->setParent($menu);
+        $menuNode->setLabel('item-1');
+        $menuNode->setName('item-1');
+        $manager->persist($menuNode);
+
+        $menuNode = new MenuNode;
+        $menuNode->setParent($menu);
+        $menuNode->setLabel('This node has a URI');
+        $menuNode->setUri('http://www.example.com');
+        $menuNode->setName('item-2');
+        $manager->persist($menuNode);
+
+        $subNode = new MenuNode;
+        $subNode->setParent($menuNode);
+        $subNode->setLabel('@todo this node should have content');
+        $subNode->setName('sub-item-1');
+        $manager->persist($subNode);
+
+        $subNode = new MenuNode;
+        $subNode->setParent($menuNode);
+        $subNode->setLabel('This node has an assigned route');
+        $subNode->setName('sub-item-2');
+        $subNode->setRoute('link_test_route');
+        $manager->persist($subNode);
+
+        $subNode = new MenuNode;
+        $subNode->setParent($menuNode);
+        $subNode->setLabel('This node has an assigned route with parameters');
+        $subNode->setName('sub-item-3');
+        $subNode->setRoute('link_test_route_with_params');
+        $subNode->setRouteParameters(array('foo' => 'bar', 'bar' => 'foo'));
+        $manager->persist($subNode);
+
+        $menuNode = new MenuNode;
+        $menuNode->setParent($menu);
+        $menuNode->setLabel('item-3');
+        $menuNode->setName('item-3');
+        $manager->persist($menuNode);
+
+        $menu = new Menu;
+        $menu->setName('another-menu');
+        $menu->setLabel('Another Menu');
+        $menu->setParent($menuRoot);
+        $manager->persist($menu);
+
+        $menuNode = new MenuNode;
+        $menuNode->setParent($menu);
+        $menuNode->setLabel('This node has uri, route and content set. but linkType is set to route');
+        $menuNode->setLinkType('route');
+        $menuNode->setUri('http://www.example.com');
+        $menuNode->setRoute('link_test_route');
+        $menuNode->setName('item-1');
+        $manager->persist($menuNode);
+
+        $menuNode = new MenuNode;
+        $menuNode->setParent($menu);
+        $menuNode->setLabel('item-2');
+        $menuNode->setName('item-2');
+        $manager->persist($menuNode);
+
+        $manager->flush();
+    }
+}
--- Tests/Resources/app/config/routing.php
+++ Tests/Resources/app/config/routing.php
@@ -4,7 +4,10 @@ use Symfony\Component\Routing\RouteCollection;
 
 $collection = new RouteCollection();
 $collection->addCollection(
-    $loader->import(CMF_TEST_CONFIG_DIR.'/routing/sonata_routing.yml'
-));
+    $loader->import(CMF_TEST_CONFIG_DIR.'/routing/sonata_routing.yml')
+);
+$collection->addCollection(
+    $loader->import(__DIR__.'/routing/test.yml')
+);
 
 return $collection;
