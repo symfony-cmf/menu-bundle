d7df7ac0d5c5529d94f2d235bfcf2b570d5ba3fb
dantleech <dan.t.leech@gmail.com>
2013-07-21 10:55:13 +0100

    Added test for create

--- Tests/WebTest/Admin/MenuAdminTest.php
+++ Tests/WebTest/Admin/MenuAdminTest.php
@@ -35,5 +35,21 @@ class MenuAdminTest extends BaseTestCase
         $crawler = $this->client->request('GET', '/admin/cmf/menu/menu/create');
         $res = $this->client->getResponse();
         $this->assertEquals(200, $res->getStatusCode());
+
+        $button = $crawler->selectButton('Create');
+        $form = $button->form();
+        $node = $form->getFormNode();
+        $actionUrl = $node->getAttribute('action');
+        $uniqId = substr(strchr($actionUrl, '='), 1);
+
+        $form[$uniqId.'[parent]'] = '/test/menus';
+        $form[$uniqId.'[name]'] = 'foo-test';
+        $form[$uniqId.'[label]'] = 'Foo Test';
+
+        $this->client->submit($form);
+        $res = $this->client->getResponse();
+
+        // If we have a 302 redirect, then all is well
+        $this->assertEquals(302, $res->getStatusCode());
     }
 }
