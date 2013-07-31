9d524347be9096499240dfaaae2379635250c300
Lukas Kahwe Smith <smith@pooteeweet.org>
2013-07-31 20:30:04 +0200

    fix interfaces names

--- Tests/Resources/Document/Content.php
+++ Tests/Resources/Document/Content.php
@@ -2,17 +2,19 @@
 
 namespace Symfony\Cmf\Bundle\MenuBundle\Tests\Resources\Document;
 
-use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
-use Symfony\Cmf\Bundle\MenuBundle\Model\MenuNodeReferrersInterface;
-use Knp\Menu\NodeInterface;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ODM\PHPCR\Mapping\Annotations as PHPCRODM;
+
+use Knp\Menu\NodeInterface;
+
 use Symfony\Component\Routing\Route;
-use Symfony\Cmf\Component\Routing\RouteReferrersInterface;
+use Symfony\Cmf\Component\Routing\RouteReferrersReadInterface;
+use Symfony\Cmf\Bundle\MenuBundle\Model\MenuNodeReferrersInterface;
 
 /**
  * @PHPCRODM\Document(referenceable=true)
  */
-class Content implements MenuNodeReferrersInterface, RouteReferrersInterface
+class Content implements MenuNodeReferrersInterface, RouteReferrersReadInterface
 {
     /**
      * @PHPCRODM\Id()
