1244d76a4f7a92dcef27d3912c56782985880530
Sjoerd Peters <speters@netvlies.nl>
2013-08-06 23:29:48 +0200

    renamed MenuNodeCommon to MenuNodeCommonAdmin

--- Admin/MenuAdmin.php
+++ Admin/MenuAdmin.php
@@ -4,7 +4,7 @@ namespace Symfony\Cmf\Bundle\MenuBundle\Admin;
 
 use Sonata\AdminBundle\Form\FormMapper;
 
-class MenuAdmin extends MenuNodeCommon
+class MenuAdmin extends MenuNodeCommonAdmin
 {
     protected $baseRouteName = 'cmf_menu';
     protected $baseRoutePattern = '/cmf/menu/menu';
