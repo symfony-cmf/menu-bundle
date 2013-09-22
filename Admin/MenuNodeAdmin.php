904a93dd10b3cd5fa6ca70ce18effe8dc67f15d5
Ben Glassman <ben@vtdesignworks.com>
2013-09-22 18:54:40 -0400

    Simplify breadcrumb code to allow for non-Menu MenuNode parents.

--- Admin/MenuNodeAdmin.php
+++ Admin/MenuNodeAdmin.php
@@ -5,6 +5,7 @@ namespace Symfony\Cmf\Bundle\MenuBundle\Admin;
 use Sonata\AdminBundle\Form\FormMapper;
 use Sonata\DoctrinePHPCRAdminBundle\Admin\Admin;
 use Symfony\Cmf\Bundle\MenuBundle\Model\MenuNode;
+use Symfony\Cmf\Bundle\MenuBundle\Model\Menu;
 use Knp\Menu\ItemInterface as MenuItemInterface;
 use Doctrine\Common\Util\ClassUtils;
 
@@ -41,71 +42,29 @@ class MenuNodeAdmin extends AbstractMenuNodeAdmin
             return $menuNodeNode;
         }
 
-        $menuDoc = $this->getMenuForSubject($this->getSubject());
+        $parentDoc = $this->getSubject()->getParent();
         $pool = $this->getConfigurationPool();
-        $menuAdmin = $pool->getAdminByClass(
-            ClassUtils::getClass($menuDoc)
+        $parentAdmin = $pool->getAdminByClass(
+            ClassUtils::getClass($parentDoc)
         );
-        $menuAdmin->setSubject($menuDoc);
-        $menuEditNode = $menuAdmin->buildBreadcrumbs($action, $menu);
-        if ($menuAdmin->isGranted('EDIT' && $menuAdmin->hasRoute('edit'))) {
-            $menuEditNode->setUri(
-                $menuAdmin->generateUrl('edit', array(
-                    'id' => $this->getUrlsafeIdentifier($menuDoc)
+
+        if (null === $parentAdmin) {
+            return $menuNodeNode;
+        }
+
+        $parentAdmin->setSubject($parentDoc);
+        $parentEditNode = $parentAdmin->buildBreadcrumbs($action, $menu);
+        if ($parentAdmin->isGranted('EDIT' && $parentAdmin->hasRoute('edit'))) {
+            $parentEditNode->setUri(
+                $parentAdmin->generateUrl('edit', array(
+                    'id' => $this->getUrlsafeIdentifier($parentDoc)
                 ))
             );
         }
 
         $menuNodeNode->setParent(null);
-        $current = $menuEditNode->addChild($menuNodeNode);
+        $current = $parentEditNode->addChild($menuNodeNode);
 
         return $current;
-    }
-
-    protected function getMenuForSubject(MenuNode $subject)
-    {
-        $id = $subject->getId();
-
-        $menuId = $this->getMenuIdForNodeId($id);
-
-        $menu = $this->modelManager->find(null, $menuId);
-
-        return $menu;
-    }
-
-    protected function getMenuIdForNodeId($id)
-    {
-        // I wonder if this could be simplified in Phpcr/PathHelper
-        //
-        // $relPath = PathHelper::removeBasePath($this->menuRoot, $id);
-        // $menuId = PathHelper:splicePath($relPath, 0, 1);
-
-        if (0 !== strpos($id, $this->menuRoot)) {
-            throw new \InvalidArgumentException(sprintf(
-                'Cannot find base path "%s" in menu node ID "%s"', $this->menuRoot, $id
-            ));
-        }
-
-        $relPath = substr($id, strlen($this->menuRoot) + 1);
-        $parts = explode('/', $relPath);
-
-        if (count($parts) == 0) {
-            throw new \InvalidArgumentException(sprintf(
-                'ID for menu node "%s" may not be the same as root path "%s"',
-                $id, $this->menuRoot
-            ));
-        }
-
-        if (count($parts) == 1) {
-            throw new \InvalidArgumentException(sprintf(
-                'MenuNode "%s" may not hold the position reserved for a Menu.',
-                $id
-            ));
-        }
-
-        $first = $parts[0];
-        $menuId = sprintf('%s/%s', $this->menuRoot, $first);
-
-        return $menuId;
     }
 }
