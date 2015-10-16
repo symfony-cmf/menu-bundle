2aecf714649273db104e002d6787d255183b190e
WouterJ <waldio.webdesign@gmail.com>
2015-10-17 00:16:23 +0200

    Update year in the license headers

--- Tests/Unit/QuietFactoryTest.php
+++ Tests/Unit/QuietFactoryTest.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Cmf\Bundle\MenuBundle\Tests\Unit;
 
 use Symfony\Cmf\Bundle\MenuBundle\QuietFactory;
