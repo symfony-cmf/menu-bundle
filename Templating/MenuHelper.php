cd4e821821a2b889fe5e83a037984ace246b6c16
WouterJ <waldio.webdesign@gmail.com>
2016-01-09 16:22:07 +0100

    Register as services

--- Templating/MenuHelper.php
+++ Templating/MenuHelper.php
@@ -1,11 +1,20 @@
 <?php
 
+/*
+ * This file is part of the Symfony CMF package.
+ *
+ * (c) 2011-2015 Symfony CMF
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace Symfony\Cmf\Bundle\MenuBundle\Templating;
 
 use Knp\Menu\ItemInterface;
 use Knp\Menu\NodeInterface;
 use Knp\Menu\FactoryInterface;
-use Doctrine\ODM\PHPCR\DocumentManager;
+use Doctrine\Common\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Templating\Helper\Helper;
 use Symfony\Cmf\Component\Routing\RouteObjectInterface;
@@ -20,31 +29,43 @@ use Symfony\Cmf\Bundle\MenuBundle\Model\MenuNode;
 class MenuHelper extends Helper
 {
     /**
-     * @var DocumentManager
+     * @var ManagerRegistry
      */
-    private $documentManager;
+    private $managerRegistry;
 
     /**
      * @var FactoryInterface
      */
     private $menuFactory;
+    private $managerName;
     private $contentObjectKey;
     private $routeNameKey;
 
     /**
-     * @param DocumentManager  $documentManager
+     * @param ManagerRegistry  $managerRegistry
      * @param FactoryInterface $menuFactory
      * @param string           $contentObjectKey The name of the request attribute holding
      *                                           the current content object
      * @param string           $routeNameKey     The name of the request attribute holding
      *                                           the name of the current route
      */
-    public function __construct(DocumentManager $documentManager, FactoryInterface $menuFactory, $contentObjectKey = RouteObjectInterface::CONTENT_OBJECT, $routeNameKey = RouteObjectInterface::ROUTE_NAME)
+    public function __construct(ManagerRegistry $managerRegistry, FactoryInterface $menuFactory, $contentObjectKey = RouteObjectInterface::CONTENT_OBJECT, $routeNameKey = RouteObjectInterface::ROUTE_NAME)
     {
-        $this->documentManager = $documentManager;
+        $this->managerRegistry = $managerRegistry;
         $this->menuFactory = $menuFactory;
         $this->contentObjectKey = $contentObjectKey;
         $this->routeNameKey = $routeNameKey;
+    }
+
+    /**
+     * Set the object manager name to use for this loader. If not set, the
+     * default manager as decided by the manager registry will be used.
+     *
+     * @param string|null $managerName
+     */
+    public function setManagerName($managerName)
+    {
+        $this->managerName = $managerName;
     }
 
     /**
@@ -80,49 +101,62 @@ class MenuHelper extends Helper
     /**
      * Tries to find the current item from the request.
      *
+     * The returned item does *not* include the parent and children,
+     * in order to minimalize the overhead.
+     *
      * @param Request $request
      *
-     * @return MenuNode|null
+     * @return ItemInterface|null
      */
     public function getCurrentItem(Request $request)
     {
         $node = $this->getCurrentNode($request);
 
         if (!$node instanceof NodeInterface) {
-            return null;
+            return;
         }
 
         return $this->menuFactory->createItem($node->getName(), $node->getOptions());
     }
 
+    /**
+     * Retrieves the current node based on a Request.
+     *
+     * It uses some special Request attributes that are managed by
+     * the CmfRoutingBundle:
+     *
+     *  * RouteObjectInterface::CONTENT_OBJECT to match a menu node by the refering content
+     *  * RouteObjectInterface::ROUTE_NAME to match a menu node by the refering route name
+     *
+     * @return NodeInterface|null
+     */
     public function getCurrentNode(Request $request)
     {
-        $node = null;
         if ($request->attributes->has($this->contentObjectKey)) {
             $content = $request->attributes->get($this->contentObjectKey);
 
-            $node = $this->documentManager->getRepository('CmfMenuBundle:MenuNode')
-                ->findOneBy(array('content' => $content));
-        } elseif ($request->attributes->has($this->routeNameKey)) {
+            return $this->managerRegistry->getManager($this->managerName)->getRepository('CmfMenuBundle:MenuNode')->findOneBy(array('content' => $content));
+        }
+        
+        if ($request->attributes->has($this->routeNameKey)) {
             $route = $request->attributes->get($this->routeNameKey);
 
-            $nodes = $this->documentManager->getRepository('CmfMenuBundle:MenuNode')
-                ->findBy(array('route' => $route));
+            $nodes = $this->managerRegistry->getManager($this->managerName)->getRepository('CmfMenuBundle:MenuNode')->findBy(array('route' => $route));
             if (1 === count($nodes)) {
-                $node = $nodes->first();
+                return $nodes->first();
             } else {
-                foreach ($nodes as $n) {
-                    if ('route' === $n->getLinkType()) {
-                        $node = $n;
-                        break;
+                foreach ($nodes as $node) {
+                    if ('route' === $node->getLinkType()) {
+                        return $node;
                     }
                 }
             }
         }
-
-        return $node;
     }
 
+    /**
+     * {@inheritdoc}
+     */
     public function getName()
     {
         return 'cmf_menu';
